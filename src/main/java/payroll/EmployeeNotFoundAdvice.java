1df8bd68378b9b3f3a296e6136fa62a5b6396a90
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2022-01-20 08:43:23 +0100

    With my comments.

--- src/main/java/payroll/EmployeeNotFoundAdvice.java
+++ src/main/java/payroll/EmployeeNotFoundAdvice.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class EmployeeNotFoundAdvice {
 
+    // @ResponseBody sygnalizuje, że wyjątek jest przekazywany bezpośrednio do treści odpowiedzi
     @ResponseBody
+    // @ExceptionHandler konfiguruje poradę tak, aby odpowiadała tylko wtedy, gdy EmployeeNotFoundException zostanie zgłoszony.
     @ExceptionHandler(EmployeeNotFoundException.class)
+    // @ResponseStatus mówi, aby wydać HttpStatus.NOT_FOUND, tj. HTTP 404
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String employeeNotFoundHandler(EmployeeNotFoundException exception) {
         return exception.getMessage();
--- src/main/java/payroll/EmployeeNotFoundException.java
+++ src/main/java/payroll/EmployeeNotFoundException.java
@@ -1,5 +1,7 @@
 package payroll;
 
+// Gdy EmployeeNotFoundException zostanie wyrzucony, dodatkowo id
+// oraz z konfiguracji Spring MVC jest używana do renderowania HTTP 404: EmployeeNotFoundAdvice
 public class EmployeeNotFoundException extends RuntimeException {
 
     public EmployeeNotFoundException (Long id) {
